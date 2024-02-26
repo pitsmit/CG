5cb94e18b9882f376af266dc7046360c53a6e5fa
pitsmit <petr_smirnov_2004@mail.ru>
2024-02-26 18:27:03 +0300

    1_lab

---
+++ lab_01/src/math.js
@@ -0,0 +1,71 @@
+export function search_centeres(arr) {
+    var circles_data = [];
+    for (var i = 0; i < arr.length; i++){
+        var p1x = arr[i][0][0];
+        var p1y = arr[i][0][1];
+        var p2x = arr[i][1][0];
+        var p2y = arr[i][1][1];
+        var p3x = arr[i][2][0];
+        var p3y = arr[i][2][1];
+
+        const a = p2x - p1x;
+        const b = p2y - p1y;
+    
+        const c = p3x - p1x;
+        const d = p3y - p1y;
+    
+        const g = 2 * (a * (p3y - p2y) - b * (p3x - p2x));
+    
+        const e = a * (p1x + p2x) + b * (p1y + p2y);
+        const f = c * (p1x + p3x) + d * (p1y + p3y);
+    
+        const centerX = (d * e - b * f) / g;
+        const centerY = (a * f - c * e) / g;
+        const radius = Math.sqrt(Math.pow(p1x - centerX, 2) + Math.pow(p1y - centerY, 2));
+
+        circles_data.push([centerX, centerY, radius]);
+    }
+
+    return circles_data;
+}
+
+
+export function search_corners(centers_1, centers_2){
+    var final_data = [];
+
+    for (var i = 0; i < centers_1.length; i++){
+        for (var j = 0; j < centers_2.length; j++){
+            var center1_x = centers_1[i][0];
+            var center1_y = centers_1[i][1];
+            var center1_R = centers_1[i][2];
+            var center2_x = centers_2[j][0];
+            var center2_y = centers_2[j][1];
+            var center2_R = centers_2[j][2];
+
+            var tan = (center2_y - center1_y) / (center2_x - center1_x);
+
+            var radians_on_X = Math.atan(tan);
+
+            var degree_on_X = (180 * radians_on_X) / Math.PI;
+            var result_angle = 90 - degree_on_X;
+
+            var res_data = [[center1_x, center1_y, center1_R], [center2_x, center2_y, center2_R], result_angle];
+            final_data.push(res_data);
+        }
+    }
+    return final_data;
+}
+
+
+export function search_min_angle(data){
+    var res = [[], [], 0];
+    var min_angle = data[0][2];
+    for (var i = 0; i < data.length; i++){
+        if (data[i][2] <= min_angle){
+            res = data[i];
+            min_angle = data[i][2];
+        }
+    }
+
+    return res;
+}
