ae3120755276148509c3335da424554347f2be3f
pitsmit <petr_smirnov_2004@mail.ru>
2024-03-25 16:29:55 +0300

    add first func tests to 3-4 labs

---
+++ lab_03/func-tests/test.test.js
@@ -0,0 +1,163 @@
+import { test, expect } from '@playwright/test';
+
+test('библиотечная функция построения', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/index.html`);
+  await page.getByRole('link', { name: '2' }).click();
+  await page.locator('input').first().check();
+  await page.locator('input[name="len"]').click();
+  await page.locator('input[name="len"]').fill('300');
+  await page.locator('input[name="shag"]').click();
+  await page.locator('input[name="shag"]').fill('5');
+  await page.getByLabel('Выберите цвет отрезка').click();
+  await page.getByLabel('Выберите цвет отрезка').fill('#ff0000');
+  await page.getByLabel('Выберите цвет фона').click();
+  await page.getByLabel('Выберите цвет фона').fill('#000000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+  await page.screenshot({ path: './results/1.png', fullPage: true});
+});
+
+
+
+test('Алгоритм ЦДА', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input:nth-child(8)').check();
+  await page.locator('input[name="len"]').click();
+  await page.locator('input[name="len"]').fill('200');
+  await page.locator('input[name="shag"]').click();
+  await page.locator('input[name="shag"]').fill('5');
+  await page.getByLabel('Выберите цвет отрезка').click();
+  await page.getByLabel('Выберите цвет отрезка').fill('#ff0000');
+  await page.getByLabel('Выберите цвет фона').click();
+  await page.getByLabel('Выберите цвет фона').fill('#000000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+  await page.screenshot({ path: './results/2.png', fullPage: true});
+});
+
+
+test('Брезенхем с действительными числами', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input:nth-child(11)').check();
+  await page.locator('input[name="len"]').click();
+  await page.locator('input[name="len"]').fill('200');
+  await page.locator('input[name="shag"]').click();
+  await page.locator('input[name="shag"]').fill('5');
+  await page.getByLabel('Выберите цвет отрезка').click();
+  await page.getByLabel('Выберите цвет отрезка').fill('#ff0000');
+  await page.getByLabel('Выберите цвет фона').click();
+  await page.getByLabel('Выберите цвет фона').fill('#000000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+  await page.screenshot({ path: './results/3.png', fullPage: true});
+});
+
+
+test('Брезенхем с целыми числами', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input:nth-child(14)').check();
+  await page.locator('input[name="len"]').click();
+  await page.locator('input[name="len"]').fill('200');
+  await page.locator('input[name="shag"]').click();
+  await page.locator('input[name="shag"]').fill('5');
+  await page.getByLabel('Выберите цвет отрезка').click();
+  await page.getByLabel('Выберите цвет отрезка').fill('#ff0000');
+  await page.getByLabel('Выберите цвет фона').click();
+  await page.getByLabel('Выберите цвет фона').fill('#000000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+  await page.screenshot({ path: './results/4.png', fullPage: true});
+});
+
+
+test('Брезенхем с устранением ступенчатости', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input:nth-child(17)').check();
+  await page.locator('input[name="len"]').click();
+  await page.locator('input[name="len"]').fill('200');
+  await page.locator('input[name="shag"]').click();
+  await page.locator('input[name="shag"]').fill('5');
+  await page.getByLabel('Выберите цвет отрезка').click();
+  await page.getByLabel('Выберите цвет отрезка').fill('#ff0000');
+  await page.getByLabel('Выберите цвет фона').click();
+  await page.getByLabel('Выберите цвет фона').fill('#000000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+  await page.screenshot({ path: './results/5.png', fullPage: true});
+});
+
+
+test('Алгоритм ВУ', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input:nth-child(20)').check();
+  await page.locator('input[name="len"]').click();
+  await page.locator('input[name="len"]').fill('200');
+  await page.locator('input[name="shag"]').click();
+  await page.locator('input[name="shag"]').fill('5');
+  await page.getByLabel('Выберите цвет отрезка').click();
+  await page.getByLabel('Выберите цвет отрезка').fill('#ff0000');
+  await page.getByLabel('Выберите цвет фона').click();
+  await page.getByLabel('Выберите цвет фона').fill('#000000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+  await page.screenshot({ path: './results/6.png', fullPage: true});
+});
+
+
+test('Исследование временных характеристик', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/timers_diagrams.html`);
+  await page.getByRole('button', { name: 'Получить данные о временных характеристиках' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+
+  await page.screenshot({ path: './results/7.png', fullPage: true});
+});
+
+
+test('Исследование ступенчатых характеристик', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/step_researchers.html`);
+  await page.getByRole('textbox').click();
+  await page.getByRole('textbox').fill('30');
+  await page.getByRole('button', { name: 'Получить данные о ступенчатых характеристиках' }).click();
+  
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+
+  await page.screenshot({ path: './results/8.png', fullPage: true});
+});
---
+++ lab_04/func-tests/test.test.js
@@ -0,0 +1,108 @@
+import { test, expect } from '@playwright/test';
+
+test('lib-circle', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/index.html`);
+  await page.locator('input[name="state"]').first().check();
+  await page.locator('input[name="x"]').click();
+  await page.locator('input[name="x"]').fill('0');
+  await page.locator('input[name="y"]').click();
+  await page.locator('input[name="y"]').fill('0');
+  await page.locator('input[name="fig"]').first().check();
+  await page.getByLabel('Радиус:').click();
+  await page.getByLabel('Радиус:').fill('100');
+  await page.getByLabel('Выберите цвет фигуры').click();
+  await page.getByLabel('Выберите цвет фигуры').fill('#ff0000');
+  await page.getByRole('button', { name: 'Применить' }).click();
+
+  await page.screenshot({ path: './results/1.png', fullPage: true});
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+});
+
+
+test('brez-ellipse-degree', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input[name="state"]').nth(1).check();
+  await page.locator('input[name="x"]').click();
+  await page.locator('input[name="x"]').fill('0');
+  await page.locator('input[name="y"]').click();
+  await page.locator('input[name="y"]').fill('0');
+  await page.getByLabel('Выберите цвет фигуры').click();
+  await page.getByLabel('Выберите цвет фигуры').fill('#ea0b0b');
+  await page.getByText('Шаг изменения радиуса:').click();
+  await page.locator('input[name="rdelta"]').click();
+  await page.locator('input[name="rdelta"]').fill('10');
+  await page.locator('input[name="countfig"]').click();
+  await page.locator('input[name="countfig"]').fill('20');
+  await page.locator('input[name="fig"]').nth(1).check();
+  await page.getByLabel('A(по горизонтали):').click();
+  await page.getByLabel('A(по горизонтали):').fill('10');
+  await page.getByLabel('B(по вертикали):').click();
+  await page.getByLabel('B(по вертикали):').fill('15');
+  await page.getByRole('button', { name: 'Применить' }).click();
+
+  await page.screenshot({ path: './results/2.png', fullPage: true});
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+});
+
+
+test('param-ellipse-degree', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/degree_researchers.html`);
+  await page.locator('input[name="state"]').nth(3).check();
+  await page.locator('input[name="x"]').click();
+  await page.locator('input[name="x"]').fill('0');
+  await page.locator('input[name="y"]').click();
+  await page.locator('input[name="y"]').fill('0');
+  await page.locator('input[name="rdelta"]').click();
+  await page.locator('input[name="rdelta"]').fill('20');
+  await page.locator('input[name="countfig"]').click();
+  await page.locator('input[name="countfig"]').click();
+  await page.locator('input[name="countfig"]').click();
+  await page.locator('input[name="countfig"]').fill('20');
+  await page.locator('input[name="fig"]').nth(1).check();
+  await page.getByLabel('A(по горизонтали):').click();
+  await page.getByLabel('A(по горизонтали):').fill('5');
+  await page.getByLabel('B(по вертикали):').click();
+  await page.getByLabel('B(по вертикали):').fill('30');
+  await page.getByRole('button', { name: 'Применить' }).click();
+
+  await page.screenshot({ path: './results/3.png', fullPage: true});
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+});
+
+
+test('canon-circle-degree', async ({ page }) => {
+  var t0 = performance.now();
+
+  await page.goto(`file://${__dirname}/../out/index.html`);
+  await page.getByRole('link', { name: '2' }).click();
+  await page.locator('input[name="state"]').nth(2).check();
+  await page.locator('input[name="x"]').click();
+  await page.locator('input[name="x"]').fill('0');
+  await page.locator('input[name="y"]').click();
+  await page.locator('input[name="y"]').fill('0');
+  await page.locator('input[name="rdelta"]').click();
+  await page.locator('input[name="rdelta"]').fill('10');
+  await page.locator('input[name="countfig"]').click();
+  await page.locator('input[name="countfig"]').fill('10');
+  await page.locator('input[name="fig"]').first().check();
+  await page.getByLabel('Радиус:').click();
+  await page.getByLabel('Радиус:').fill('30');
+  await page.getByRole('button', { name: 'Применить' }).click();
+
+  await page.screenshot({ path: './results/4.png', fullPage: true});
+
+  var t1 = performance.now();
+  console.log(t1 - t0);
+});
